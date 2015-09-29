71df0b29ae2a014c406b6f1c1db31808324ad8ce
kpurvesh <kpurvesh@gmail.com>
2015-09-29 01:39:41 -0500

    hw3 part 2 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -23,10 +23,34 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings_arr = rating_list.split(",")
+  ratings_arr.each {|rating|
+			if uncheck.eql?(nil)
+				step "I check \"ratings_" + rating + "\""
+			elsif uncheck.eql?("un")
+				step "I uncheck \"ratings_" + rating + "\""
+			end
+		   }		
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  value = Movie.count()
+  page.all("table#movies tbody tr").count.should == value
 end
+
+Then /I should (.*) movies with following ratings: (.*)/ do |see, rating_list|
+	ratings_arr = rating_list.split(",")
+	ratings_arr.each {|rating|
+			titles = Movie.where(rating: rating).pluck(:title)
+			titles.each {|title|
+				if see.eql?("see")
+					step "I should see \"" + title + "\""
+				elsif see.eql?("not see")
+					step "I should not see \"" + title + "\""
+				end
+			}
+	}
+end
+
+
